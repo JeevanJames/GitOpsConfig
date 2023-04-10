7a3c36c65c202ca4f94377632ba7a87d04f6aa37
Jeevan James <jeevanjj@live.com>
2023-04-10 21:49:43 +0530

    Update test harness

--- testharness/Cli/Program.cs
+++ testharness/Cli/Program.cs
@@ -1,8 +1,11 @@
+using System.Linq.Expressions;
+
 using GitOpsConfig;
 
-using IniFile;
+using Newtonsoft.Json.Linq;
 
 using Spectre.Console;
+using Spectre.Console.Json.Syntax;
 
 using static Spectre.Console.AnsiConsole;
 using static Spectre.Console.Color;
@@ -10,49 +13,77 @@ using static Spectre.Console.Color;
 const string rootDir = @"D:\Temp\config";
 string appsDir = Path.Combine(rootDir, "apps");
 string outputDir = Path.Combine(rootDir, "_processing", "output");
+string referenceDir = Path.Combine(rootDir, "_processing", "references");
 
+// Ensure output directory exists and delete any files in it.
 if (!Directory.Exists(outputDir))
     Directory.CreateDirectory(outputDir);
+string[] outputFiles = Directory.GetFiles(outputDir, "*", SearchOption.TopDirectoryOnly);
+foreach (string outputFile in outputFiles)
+    File.Delete(outputFile);
+
+if (!Directory.Exists(referenceDir))
+    Directory.CreateDirectory(referenceDir);
 
 ConfigurationBuilder builder = new(rootDir);
 
 IEnumerable<string[]> sectionSets = SectionDiscoverer.DiscoverSectionsFrom(appsDir);
 
 Table table = new Table()
-    .AddColumns("App", "Sections", "Result");
+    .AddColumns("File name", "Comparison");
 
 foreach (string[] sectionSet in sectionSets)
 {
-    string sectionPath = $"[{Cyan1}]{string.Join($"[{Magenta1}] -> [/]", sectionSet)}[/]";
-
-    string currentAppName = "N/A";
     try
     {
         await foreach ((string appName, IReadOnlyList<GeneratedConfiguration> configs) in
                        builder.GenerateAsync(sectionSet))
         {
-            currentAppName = appName;
-
             foreach (GeneratedConfiguration config in configs)
             {
                 string sectionStr = string.Join('.', sectionSet);
-                string fileName = $"{currentAppName}_{sectionStr}_{config.FileName}";
+                string displayFileName = $"[{Green1}]{appName}[/]_[{Magenta1}]{sectionStr}[/]_[{Yellow1}]{config.FileName}[/]";
+
+                string fileName = $"{appName}_{sectionStr}_{config.FileName}";
                 string filePath = Path.Combine(outputDir, fileName);
+
                 await File.WriteAllTextAsync(filePath, config.Content);
 
-                table.AddRow(currentAppName, sectionPath, $"[{Green1}]{fileName.EscapeMarkup()}[/]");
+                string result = CompareConfigs(outputDir, referenceDir, fileName) switch
+                {
+                    null => $"[{Orange1}]Reference file not found[/]",
+                    true => $"[{Green1}]Semantically equal[/]",
+                    false => $"[{Yellow4}]Different[/]",
+                };
+
+                table.AddRow(
+                    new Markup(displayFileName),
+                    new Markup(result));
             }
         }
     }
     catch (Exception ex)
     {
         table.AddRow(
-            new Text(currentAppName),
-            new Markup(sectionPath),
-            ex.GetRenderable(ExceptionFormats.ShortenEverything));
+            new Markup(string.Join('.', sectionSet)),
+            new Markup($"[{Red1}]{ex.Message}[/]"));
     }
 }
 
 Write(table);
 
 System.Console.ReadLine();
+
+static bool? CompareConfigs(string outputDir, string referenceDir, string fileName)
+{
+    string referenceFilePath = Path.Combine(referenceDir, referenceDir);
+    if (!File.Exists(referenceFilePath))
+        return null;
+
+    string outputFilePath = Path.Combine(outputDir, fileName);
+
+    JObject reference = JObject.Parse(File.ReadAllText(referenceFilePath));
+    JObject output = JObject.Parse(File.ReadAllText(outputFilePath));
+
+    return JToken.DeepEquals(output, reference);
+}
