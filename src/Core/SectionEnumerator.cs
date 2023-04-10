07803160f7ee1f578cddfc063c88a8c4932164a9
Jeevan James <jeevanjj@live.com>
2023-04-10 17:40:34 +0530

    Add SectionEnumerator utility class

---
+++ src/Core/SectionEnumerator.cs
@@ -0,0 +1,33 @@
+﻿namespace GitOpsConfig;
+
+public static class SectionEnumerator
+{
+    public static IEnumerable<string[]> EnumerateSectionsFrom(string rootDir)
+    {
+        List<string[]> chains = new();
+
+        foreach (string subdir in Directory.EnumerateDirectories(rootDir, "*", SearchOption.TopDirectoryOnly))
+        {
+            EnumerateSubdir(subdir, chains, new Stack<string>());
+        }
+
+        return chains;
+    }
+
+    private static void EnumerateSubdir(string dir, List<string[]> chains, Stack<string> currentChain)
+    {
+        currentChain.Push(Path.GetFileName(dir));
+
+        IEnumerable<string> subdirs = Directory.EnumerateDirectories(dir, "*", SearchOption.TopDirectoryOnly);
+        if (subdirs.Any())
+        {
+            foreach (string subdir in subdirs)
+                EnumerateSubdir(subdir, chains, currentChain);
+        }
+        else
+        {
+            chains.Add(currentChain.Reverse().ToArray());
+            currentChain.Pop();
+        }
+    }
+}
--- testharness/Cli/Program.cs
+++ testharness/Cli/Program.cs
@@ -1,12 +1,16 @@
 using GitOpsConfig;
 
+using IniFile;
+
 using Spectre.Console;
 using Spectre.Console.Json;
 
 using static Spectre.Console.AnsiConsole;
 using static Spectre.Console.Color;
 
-ConfigurationBuilder builder = new(@"D:\Temp\config");
+const string rootDir = @"D:\Temp\config";
+
+//ConfigurationBuilder builder = new(rootDir);
 
 //await foreach ((string fileName, string content) in builder.GenerateAsync("MyApp", "dev"))
 //{
@@ -14,21 +18,28 @@ ConfigurationBuilder builder = new(@"D:\Temp\config");
 //    Write(new JsonText(content));
 //}
 
-await foreach ((string appName, IReadOnlyList<GeneratedConfiguration> configs)
-    in builder.GenerateAsync(new[] { "qa", "in-qa" }))
+//await foreach ((string appName, IReadOnlyList<GeneratedConfiguration> configs)
+//    in builder.GenerateAsync(new[] { "qa", "in-qa" }))
+//{
+//    Write(new Rule(appName).RuleStyle(new Style(Yellow1)));
+//    foreach (GeneratedConfiguration config in configs)
+//    {
+//        JsonText jsonText = new JsonText(config.Content)
+//            .BracketColor(Yellow3)
+//            .BracesColor(Yellow1);
+//        Write(new Panel(jsonText)
+//            .Header(config.FileName)
+//            .BorderColor(Cyan1)
+//            .DoubleBorder()
+//            .Expand());
+//    }
+//}
+
+IEnumerable<string[]> sectionSets = SectionEnumerator.EnumerateSectionsFrom(rootDir);
+foreach (string[] section in sectionSets)
 {
-    Write(new Rule(appName).RuleStyle(new Style(Yellow1)));
-    foreach (GeneratedConfiguration config in configs)
-    {
-        JsonText jsonText = new JsonText(config.Content)
-            .BracketColor(Yellow3)
-            .BracesColor(Yellow1);
-        Write(new Panel(jsonText)
-            .Header(config.FileName)
-            .BorderColor(Cyan1)
-            .DoubleBorder()
-            .Expand());
-    }
+    string path = string.Join($"[{Yellow1}]/[/]", section);
+    MarkupLine($"[{Green1}]{path}[/]");
 }
 
 System.Console.ReadLine();
